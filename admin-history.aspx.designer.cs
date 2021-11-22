84619d137715862e7230392d1ec5c2dece2d9612
sahildongre20 <sahildongre1000@gmail.com>
2021-11-22 20:19:11 +0530

    admin done

--- admin-history.aspx.designer.cs
+++ admin-history.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace ComplaintManagement
 {
 
 
-    public partial class admin_login
+    public partial class admin_history
     {
 
         /// <summary>
@@ -22,5 +22,14 @@ namespace ComplaintManagement
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lbl_user_name control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_user_name;
     }
 }
